1a1367aed72b40ff21926812c77bc40feeb34607
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-09-12 21:47:03 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './index.css'
+import Hero from './Pages/Hero'
+import About from './Pages/About'
+import Events from './Pages/Events'
+import Footer from './Pages/Footer'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-6xl font-bold underline">
-        Hello world
-      </h1>
+      <Hero />
+      <About />
+      <Events />
+      <Footer />
     </>
   )
 }
---
+++ src/Pages/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underline">Takshak'25</h1>
+    </div>
+  )
+}
+
+export default Hero
