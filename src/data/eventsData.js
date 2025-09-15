66399c3128f20d7b43720c6ffb27c620e23d94a6
Janetmaria <janetsaji1652@gmail.com>
2025-09-15 17:44:59 +0530

    events page

---
+++ src/data/eventsData.js
@@ -0,0 +1,48 @@
+// src/data/eventsData.js
+
+export const competitions = [
+  {
+    poster: "/assets/posters/comp1.jpg",
+    link: "https://external-site.com/competition1",
+  },
+  {
+    poster: "/assets/posters/comp2.jpg",
+    link: "https://external-site.com/competition2",
+  },
+  {
+    poster: "/assets/posters/comp3.jpg",
+    link: "https://external-site.com/competition3",
+  },
+  {
+    poster: "/assets/posters/comp4.jpg",
+    link: "https://external-site.com/competition4",
+  },
+  {
+    poster: "/assets/posters/comp5.jpg",
+    link: "https://external-site.com/competition5",
+  },
+
+];
+
+export const workshops = [
+  {
+    poster: "/assets/posters/work1.jpg",
+    link: "https://external-site.com/workshop1",
+  },
+  {
+    poster: "/assets/posters/work2.jpg",
+    link: "https://external-site.com/workshop2",
+  },
+  {
+    poster: "/assets/posters/work3.jpg",
+    link: "https://external-site.com/workshop3",
+  },
+  {
+    poster: "/assets/posters/work4.jpg",
+    link: "https://external-site.com/workshop4",
+  },
+  {
+    poster: "/assets/posters/work5.jpg",
+    link: "https://external-site.com/workshop5",
+  }
+];
